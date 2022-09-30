d89d96836967b597caad918fd6dee0f33005d3ed
Juan <solanofernando35@gmail.com>
2022-09-30 10:48:20 -0500

    Creación proyecto parroquia san pio XXX

---
+++ Sistema Parroquia San Pio XX/src/Model/Feligres.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author solan
+ */
+public class Feligres {
+    
+    private String cedula;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String estrato;
+    private String estado;
+    private Integer diezmo;
+
+    public Feligres() {
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEstrato() {
+        return estrato;
+    }
+
+    public void setEstrato(String estrato) {
+        this.estrato = estrato;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
---
+++ Sistema Parroquia San Pio XX/src/View/Main.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author solan
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        StackPane root = new StackPane();
+        
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
